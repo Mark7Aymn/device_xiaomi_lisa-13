d195008491f2049ad4c99770a324569d1af61108
Mark Aymn <mark7aymn@outlook.com>
2022-10-02 21:05:31 +0200

    cherish

--- cherish_lisa.mk
+++ cherish_lisa.mk
@@ -12,13 +12,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
 # Inherit some common PixelOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lisa
+PRODUCT_NAME := cherish_lisa
 PRODUCT_MODEL := Xiaomi 11 Lite NE
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Mark7Aymn
+    
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+    # FOD animations
+    EXTRA_UDFPS_ANIMATIONS := true
